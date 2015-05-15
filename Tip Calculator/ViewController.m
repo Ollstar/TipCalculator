d3bc3e585839a52fe14a444f587ac21a6cc4021b
Oliver Andrews <oltandrews@gmail.com>
2015-05-15 13:45:58 -0700

    fin

--- Tip Calculator/ViewController.m
+++ Tip Calculator/ViewController.m
@@ -8,7 +8,16 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate, UITextInputTraits>
+
+@property (strong, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (strong, nonatomic) IBOutlet UILabel *tipAmountLabel;
+@property (strong, nonatomic) IBOutlet UITextField *tipPercentageTextField;
+@property (strong, nonatomic) IBOutlet UISlider *tipSlider;
+
+@property (assign, nonatomic) float billBeforeTips;
+@property (assign, nonatomic) float tips;
+@property (assign, nonatomic) float tipPercentage;
 
 @end
 
@@ -17,6 +26,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.billAmountTextField.delegate = self;
+    self.tipPercentageTextField.delegate = self;
+    [self.tipSlider addTarget:self action:@selector(adjustTipSlider:) forControlEvents:UIControlEventValueChanged];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +36,28 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)calculateTip:(UIButton *)sender {
+    [self.billAmountTextField resignFirstResponder];
+    [self.tipPercentageTextField resignFirstResponder];
+    
+    self.tips = self.billBeforeTips * self.tipPercentage;
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"%.2f", self.tips];;
+}
+
+-(void)textFieldDidBeginEditing:(UITextField *)textField {
+    [textField becomeFirstResponder];
+}
+
+-(void)textFieldDidEndEditing:(UITextField *)textField {
+    if (textField == self.billAmountTextField) {
+        self.billBeforeTips = [textField.text floatValue];
+    } else if (textField == self.tipPercentageTextField) {
+        self.tipPercentage = [textField.text floatValue] / 100;
+    }
+}
+
+-(IBAction)adjustTipSlider:(UISlider *)sender {
+    self.tipPercentageTextField.text = [NSString stringWithFormat:@"%.2f", sender.value];
+    self.tipPercentage = [self.tipPercentageTextField.text floatValue] / 100;
+}
 @end
